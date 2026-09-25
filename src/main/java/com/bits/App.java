c78cd47cc45ffef0b383fc1cea50b75a208f1fff
SANDHIYA V <2022wa86177@wilp.bits-pilani.ac.in>
2026-09-25 11:12:26 +0530

    Modified AppTest for CI testing

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Sandhiya!" );
+        System.out.println( "Hello 2022wa86177" );
     }
 }
